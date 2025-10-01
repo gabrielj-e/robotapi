921f1301c4e8d4d3914112fb8b2b28b521d4197c
gabrielj-e <gabriel17j.e@gmail.com>
2025-10-01 16:06:00 -0300

    Forçando um novo deploy para corrigir o POST

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -3,17 +3,19 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*")
+@RequestMapping("/hello")
 public class HelloWorldController {
-    @GetMapping("/hello")
+    @GetMapping
     public String sayHello() {
         // O valor retornado é a resposta que será enviada ao navegador.
         return "Funcionou!!!!";
     }
-      @PostMapping("/Hello")
+    @PostMapping
     public String sayHelloPost() {
         return "Olá, mundo! (via POST)";
     }
