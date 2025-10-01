1b6c7a8f32bf61c788e9d4ec4366fc23296475fc
gabrielj-e <gabriel17j.e@gmail.com>
2025-10-01 09:24:05 -0300

    Adiciona endpoint POST para /Hello

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,9 @@ public class HelloWorldController {
     public String sayHello() {
         // O valor retornado é a resposta que será enviada ao navegador.
         return "Funcionou!!!!";
+    }
+      @PostMapping("/Hello")
+    public String sayHelloPost() {
+        return "Olá, mundo! (via POST)";
     }
 }
